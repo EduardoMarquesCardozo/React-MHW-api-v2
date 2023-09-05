a88915a418055fa4951f94c1cea64cf6ddb5d361
Eduardo M. C <eduardomarquescardozo@gmail.com>
2023-09-05 17:17:43 -0300

    feat: Header component import

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import ReactDOM from "react-dom/client";
 import { GlobalStyle, Wrapper } from "./styles/index.ts";
 import Armors from "./pages/Armors/index.tsx";
+import Header from "./components/Header/index.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Wrapper>
     <GlobalStyle />
+    <Header />
     <Armors />
   </Wrapper>
 );
