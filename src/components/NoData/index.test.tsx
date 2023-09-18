79a9317d320780dc8f059b0be90e2df994fa4080
Eduardo M. C <eduardomarquescardozo@gmail.com>
2023-09-18 14:44:32 -0300

    test: NoData

---
+++ src/components/NoData/index.test.tsx
@@ -0,0 +1,9 @@
+import { render } from "@testing-library/react";
+import NoData from ".";
+
+describe("Testing NoData Component", () => {
+  it("should render the component with a messagen", () => {
+    const { getByText } = render(<NoData message="Mensagem de teste" />);
+    expect(getByText("Mensagem de teste")).toBeTruthy();
+  });
+});
--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,13 @@ import { ArmorSetProvider } from "./contexts/armorSet/index.tsx";
 import Loader from "./components/Loader/index.tsx";
 import { PaginationProvider } from "./contexts/pagination/index.tsx";
 import { SkillProvider } from "./contexts/skills/index.tsx";
+// https://monsterhunter.fandom.com/wiki/User:YukiHerz/SVG_Icons
+// https://docs.mhw-db.com/?shell#introduction
+// https://vitejs.dev/guide/
+// https://styled-components.com/docs/basics#installation
+// https://axios-http.com/ptbr/docs/intro
+// https://monsterhunter.fandom.com/wiki/Status_Effects
+// https://feathericons.com/
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Wrapper>
